c2b6fe6798766e910e3414a1b9f36f8cec867457
Daniel Amores <carlos.amores19@outlook.com>
2024-01-17 13:04:55 -0600

    Primer commit

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,9 @@
+// Creacion de numeros
+
+const numero = 30;
+const numero1 = 20.2;
+const numero2 = -20;
+
+console.log(numero);
+console.log(numero1);
+console.log(numero2);
---
+++ 04-Numeros/js/02-app.js
@@ -0,0 +1,18 @@
+const numero = 30;
+const numero1 = 20.2;
+
+let resultado;
+
+// suma
+resultado = numero + numero1;
+
+// resta
+resultado = numero - numero1;
+
+// multiplicaicon
+resultado = numero * numero1;
+
+// modulo
+resultado = numero % numero1;
+
+console.log(resultado);
