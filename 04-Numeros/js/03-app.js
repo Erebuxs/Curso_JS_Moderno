f5849104cd77a9dfeea285c7710c571026ec145f
Daniel Amores <carlos.amores19@outlook.com>
2024-01-17 13:38:02 -0600

    Seccion 4: Numeros en JavaScript

---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,17 @@
+let resultado;
+
+// pi
+resultado = Math.PI;
+
+// responder
+resultado = Math.round(2.7);
+resultado = Math.round(2.4);
+resultado = Math.round(2.5);
+
+// redondear hacia arriba
+resultado = Math.ceil(2.1);
+
+//redondear hacia abajo
+resultado = Math.floor(2.9);
+
+console.log(resultado);
---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,11 @@
+let puntaje = 5;
+console.log(puntaje);
+
+puntaje++;
+console.log(puntaje);
+
+console.log(puntaje++);
+console.log(++puntaje);
+
+console.log(puntaje--);
+console.log(--puntaje);
